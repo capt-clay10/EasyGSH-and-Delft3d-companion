f7626afff40e9176fe0a8b9f8eec5dbc5b5c77b1
Arne <arne.knies@ifg.uni-kiel.de>
2022-02-01 15:47:41 +0100

    Removed methods for creating qgis files

--- main.py
+++ main.py
@@ -50,7 +50,7 @@ step = float(step_req)  # 2.0000000e+001  # 20 minute step # max resolution for 
 
 # %% output file
 
-bct_name = mdf_file  # take the name from the mdf file
+bct_name = mdf_file  # take the name from the mdf file # Arne: How about this and the following 2 lines in one line?
 name_with_dot = bct_name.partition('.')
 name_until_dot = name_with_dot[0]
 bct_file = '{}.bct'.format(name_until_dot)
